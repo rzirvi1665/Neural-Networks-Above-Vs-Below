8c1b13fc0f55b15cbdb626b658fc70d2adb2f9b4
rzirvi1665 <78518415+rzirvi1665@users.noreply.github.com>
2022-02-05 17:56:57 -0500

    Create SLP EXP2.py

---
+++ Neural Networks/SLP EXP2.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+from tensorflow import keras
+from createData import *
+from utilityFunctions import *
+from sklearn.utils import shuffle
+
+trainingData = np.empty(0)
+testingData = np.empty(0)
+
+numTrain = 30000
+numTest = 5000
+
+print("Right about to generate images...")
+
+createEXP2(numTrain, numTest)
+
+print("Done generating images")
+print("Currently appending data...")
+
+trainingData = appendData(trainingData, numTrain, 0, 2)
+testingData = appendData(testingData, numTest, numTrain, 2)
+
+trainingData=reshapeMLP(trainingData, numTrain)
+testingData=reshapeMLP(testingData, numTest)
+
+trainingLabels = storeLabels(numTrain)
+testingLabels = storeLabels(numTest)
+
+showRandomImage(trainingData, trainingLabels, numTrain)
+
+print("Before Shuffle: ", end=" ")
+checkShuffle(trainingLabels)
+
+trainingData, trainingLabels = shuffle(trainingData, trainingLabels)
+
+
+print("After Shuffle: ", end=" ")
+checkShuffle(trainingLabels)
+
+trainingData=preprocess(trainingData)
+testingData=preprocess(testingData)
+
+model = keras.Sequential([
+	keras.layers.Flatten(input_shape=(28, 28)),
+	keras.layers.Dense(2, activation="softmax")
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(trainingData, trainingLabels, epochs=10)  # more epochs is not always better
+
+model.summary()
+
+for finalWeights in model.trainable_variables:
+	if finalWeights.shape==(784, 2):
+		for i in range(784):
+			for j in range(2):
+				print(finalWeights[i][j])
+		heatMapSLP(finalWeights)
+		break
+
+predictions = model.predict(testingData)
+
+accuracyAbove=testData(predictions, numTest, True, testingData, testingLabels)
+accuracyBelow=testData(predictions, numTest, False, testingData, testingLabels)
+
+print("Testing accuracy (choosing above image): " + str(accuracyAbove))
+print("Testing accuracy (choosing below image): " + str(accuracyBelow))
+
+testLoss, testAccuracy = model.evaluate(testingData, testingLabels)
+
+predictCategorical(predictions, testingLabels, numTest, 5)
+
+
